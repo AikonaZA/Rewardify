379d6ffe3f88129968b857264b27f1f9a2a7b723
Chona <chonajvr@gmail.com>
2024-10-15 17:05:11 +0200

    Update NuGet Packages.

--- Rewardify.AppHost/Program.cs
+++ Rewardify.AppHost/Program.cs
@@ -1,11 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.Rewardify_ApiService>("apiservice");
+var apiService = builder.AddProject<Projects.Rewardify_ApiService>("API-Service");
 
-builder.AddProject<Projects.Rewardify_Web>("webfrontend")
+builder.AddProject<Projects.Rewardify_Web>("Rewardify-Web")
     .WithExternalHttpEndpoints()
     .WithReference(apiService);
 
-builder.AddProject<Projects.Rewardify_API>("rewardify-api");
+builder.AddProject<Projects.Rewardify_API>("Rewardify-API");
 
 builder.Build().Run();
